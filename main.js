cafcf830bd09d91477f296f4de51a6f2be430c29
alttaf hussain <alttaf.hussain@gmail.com>
2014-09-18 07:57:40 +0100

    updated to no use interval code and send requests on start and end of video only

--- main.js
+++ main.js
@@ -1,74 +1,116 @@
-var app = angular.module("exercise",[])
+var app = angular.module("exercise", [])
 
-.controller("exercise",function($scope,$interval,SendRequests) {
+	.controller("exercise", function ($scope, $interval, SendRequests) {
 
-$scope.x =1;
-function runLoop(id) {
- $interval(function() {
-	SendRequests.all(id, function(data){
-	 $scope.x = data+1;
-	});
+		$scope.x = 1;
+		$scope.vidTime = -1;
+		function runLoop(id, vidTime) {
+			$interval(function () {
+				SendRequests.all(id, function (data) {
+					$scope.x = data + 1;
+				});
 
- console.log("interval");
-}
-
-,1000);
-}
-runLoop($scope.x);
-$scope.toggleButton = function(){
-$scope.toggle = $scope.toggle?false:true;
-}
-
-
-})
-/**
- * A simple example service that returns some data.
- */
-.factory('SendRequests', ['$http', function ($http) {
-    // Might use a resource here that returns a JSON arrayhttp://54.77.223.232/
-    return {
-	all	: function (id,callback){
-        // Simple index lookup
-        var uri = "http://54.77.223.232:9000/api/things";
-        $http.get(uri).
-        success(function (data, status, headers, config) {
-          console.log("/api/notifications cs  received");        
-          console.log(data);        
-          callback(data);
-        }).
-        error(function (data, status, headers, config) {
-          console.log("/api/notifications cs error: ");
-        });
-      },
-	fake	: function (id,callback){
-			console.log(id);
-			callback(2);
+				console.log("interval");
+			}, 1000);
 		}
+		
+	//	runLoop($scope.x);
 	
-	}
-}])
-	
-.directive("myVideoManip",function() {
-linkFn = function(scope, element, attrs){
-var playVideo;
-var animateDown, animateRight, vid1, button2;
-vid1 = angular.element(element.children()[0]);
-button2 = angular.element(element.children()[1]);
-playVideo = function (){
-	// select video 1 and play it
-	//amgular.element('#video1').play();
-	console.log("button click");
-	vid1[0].play();
-	scope.toggleButton();
-	scope.$apply();
-}
+		$scope.toggleButton = function () {
+			$scope.toggle = $scope.toggle ? false : true;
+			console.log($scope.vid1);
+			
+			// SendRequests.update($scope.vid1,$scope.vidTime, function (data) {
+				// console.log("sendRequest.update");
+			// });
+		}
 
-        $(vid1).on('click', playVideo);
-        $(button2).on('click', playVideo);
-}
- return {
-        restrict: 'E',
-        link: linkFn,
-		scope:false
-    };
-});
+	})
+	/**
+	 * A simple example service that returns some data.
+	 */
+	.factory('SendRequests', ['$http', function ($http) {
+				// Might use a resource here that returns a JSON arrayhttp://54.77.223.232/
+				return {
+					update : function (id,time, callback) {
+						// Simple index lookup
+						var uri = "http://54.77.223.232:9000/api/things?id="+id+"&time="+time;
+						$http.get(uri).
+						success(function (data, status, headers, config) {
+							console.log("/api/notifications cs  received");
+							console.log(data);
+							callback(data);
+						}).
+						error(function (data, status, headers, config) {
+							console.log("/api/notifications cs error: ");
+						});
+					},
+					create : function (id, callback) {
+					// Simple index lookup
+					var uri = "http://54.77.223.232:9000/api/things?id="+id;
+					$http.post(uri).
+					success(function (data, status, headers, config) {
+						console.log("/api/notifications cs  received");
+						console.log(data);
+						callback(data);
+					}).
+					error(function (data, status, headers, config) {
+						console.log("/api/notifications cs error: ");
+					});		
+					},
+					fake : function (id, callback) {
+					console.log(id);
+					callback(2);
+					}
+
+				}
+			}
+		])
+
+	.directive("myVideoManip", function ( SendRequests) {
+		linkFn = function (scope, element, attrs) {
+			var playVideo;
+			var animateDown,
+			animateRight,
+			vid1,
+			button2;
+			vid1 = angular.element(element.children()[0]);
+			button2 = angular.element(element.children()[1]);
+
+			playVideo = function () {
+				// select video 1 and play it
+				//amgular.element('#video1').play();
+				console.log("button click");
+				vid1[0].play();
+			}
+			
+			videoChange = function () {
+				// select video 1 and play it
+				//amgular.element('#video1').play();
+				console.log("videoChange");
+				scope.vidTime = vid1[0].currentTime;
+				var time  = vid1[0].currentTime;
+				var id = vid1[0].id;
+				console.log(scope.vidTime);
+				SendRequests.update(id,time, function (data) {
+				console.log("sendRequest.create"+scope.vid1);
+			});
+				scope.toggleButton();
+				scope.$apply();
+			}
+
+			$(vid1).on('pause', videoChange);
+			$(vid1).on('play', videoChange);
+			$(button2).on('click', playVideo);
+			scope.vid1 = $(vid1)[0].id;
+			SendRequests.create(scope.vid1, function (data) {
+				console.log("sendRequest.create"+scope.vid1);
+			});
+
+		}
+		return {
+			restrict : 'E',
+			link : linkFn,
+			scope : false
+		};
+	});
